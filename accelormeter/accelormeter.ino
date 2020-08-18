3416aa4d3a54c04dada0ea36d3bb18094ad2107b
Maya Bishop <mayavbishop@gmail.com>
2020-08-18 19:22:08 -0400

    calibrate MPU6050

--- accelormeter/accelormeter.ino
+++ accelormeter/accelormeter.ino
@@ -14,30 +14,40 @@
 MPU6050 mpu;
 
 int16_t accY, accZ, gyroX, gyroRate;
-float accAngle, gyroAngle=0;
-unsigned long curTime, prevTime=0, loopTime;
+float accAngle, gyroAngle=0, currentAngle, previousAngle=0;
+float tau=0.075;
+float a=0.0;
+unsigned long curTime, prevTime=0, loopTime,dt;
 
 void setup() {
   // put your setup code here, to run once:
   mpu.initialize();
+  mpu.setYAccelOffset(149);
+  mpu.setZAccelOffset(1203);
+  mpu.setXGyroOffset(77);
   Serial.begin(115200);
 }
 
 void loop() {
+  dt = loopTime/1000;
   curTime = millis();
   loopTime = curTime - prevTime;
   prevTime = curTime;
 
   gyroX = mpu.getRotationX();
   gyroRate = map(gyroX, -32768, 32767, -250, 250);
-  gyroAngle = gyroAngle + (float)gyroRate*loopTime/1000;
+  gyroAngle = gyroAngle + (float)gyroRate*dt;
   
   accY = mpu.getAccelerationY();
   accZ = mpu.getAccelerationZ();
 
   accAngle = atan2(accY, accZ)*RAD_TO_DEG;
+  currentAngle = 0.9934 * (previousAngle + (float)gyroRate*dt) + 0.0066 * (accAngle);
   Serial.print("accelormeter angle ");
   Serial.print(accAngle);
   Serial.print(" gyroscope angle ");
-  Serial.println(gyroAngle);
+  Serial.print(gyroAngle);
+  Serial.print(" calculated angle ");
+  Serial.println(currentAngle);
+  previousAngle = currentAngle;
 }
